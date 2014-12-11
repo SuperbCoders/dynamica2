a691a2399fc35966de1b19b1886f9f5e1c0ecf9d
Alexander Borovykh <immaculate.pine@gmail.com>
2014-12-11 13:37:21 +0300

    Set port for deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{w3dev-dynamica@ono.rrv.ru}
-role :web, %w{w3dev-dynamica@ono.rrv.ru}
-role :db,  %w{w3dev-dynamica@ono.rrv.ru}
+role :app, %w{w3dev-dynamica@ono.rrv.ru:2223}
+role :web, %w{w3dev-dynamica@ono.rrv.ru:2223}
+role :db,  %w{w3dev-dynamica@ono.rrv.ru:2223}
 
 set :application, 'dev-dynamica'
 set :deploy_to, '/www/dev-dynamica.onomnenado.ru'
