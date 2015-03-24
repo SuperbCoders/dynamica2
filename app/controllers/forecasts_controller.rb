2e5d91966551c7fc18a5124c5ae753aab25daaab
Alexander Borovykh <immaculate.pine@gmail.com>
2015-03-24 14:31:18 +0100

    Fill new forecast form with options from the recent forecast

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -5,7 +5,9 @@ class ForecastsController < ApplicationController
   def new
     authorize! :forecasting, @project
     @forecast = @project.forecasts.build
-    @forecast.depth ||= 10
+    @recent_forecast = @project.recent_forecast
+    @forecast.depth = @recent_forecast.try(:depth) || 10
+    @forecast.period = @recent_forecast.try(:period) || 'day'
     @project.items.create! if @project.items.empty?
     @items = @project.items.order(created_at: :desc)
   end
