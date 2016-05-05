2c71ebc8131d19ebd4f4c59b41bea86969ad89e4
Чингиз Ауанасов <auanassov.chingiz@gmail.com>
2016-05-05 12:29:06 +0500

    Shopify scope

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,4 +10,4 @@ role :db,  %w{w3dev-dyn2@ono.rrv.ru:2223}
 
 set :application, 'dev-dynamica2'
 set :deploy_to, '/www/dev-dyn2.onomnenado.ru'
-set :branch, :fix_angular_error
+set :branch, :staging
