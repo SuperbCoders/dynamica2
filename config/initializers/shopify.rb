f8f1067be0e3c75262eeffd717ba43a157935b01
Stanislav Gernyak <stager94@gmail.com>
2016-07-15 16:12:20 +0300

    исправлена ошибка при подключении магазина 2ч

--- config/initializers/shopify.rb
+++ config/initializers/shopify.rb
@@ -1,1 +1,19 @@
 ShopifyAPI::Session.setup({api_key: Dynamica::Settings::Shopify.api_key, secret: Dynamica::Settings::Shopify.secret})
+
+module ShopifyAPI
+  class Session
+    def request_token(params)
+      return token if token
+
+      code = params['code']
+
+      response = access_token_request(code)
+
+      if response.code == "200"
+        token = JSON.parse(response.body)['access_token']
+      else
+        raise RuntimeError, response.msg
+      end
+    end
+  end
+end
